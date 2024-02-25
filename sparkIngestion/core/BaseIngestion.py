a1ba3f1f7eccb469097f1a6928739fa91a88c9ee
Vikash KUMAR CHAUHAN <vikashch1998@gmail.com>
2024-02-26 02:09:16 +0530

    Big Bang Commit - Here it begins

---
+++ sparkIngestion/core/BaseIngestion.py
@@ -0,0 +1,4 @@
+class IngestionBase:  
+    @classmethod
+    def runner():
+        raise NotImplementedError
---
+++ sparkIngestion/system/config_parser.py
@@ -0,0 +1,27 @@
+
+import os 
+import json 
+from typing import Optional, Dict, List, Tuple
+from sparkIngestion.system.custom_exceptions import NoConfigFoundError 
+
+
+def parse_config(src_system: str, schema_name: str, table_name: str) -> dict:
+    """
+    This function parses the json config and returns it as a dictionary.
+    @Args:
+        -db_name(str) : The name of the database 
+        -tb_name(str) : The name of the table to be created 
+    @Returns:
+        -dict_config(dict) : JSON Config parsed as a dictionary
+    @Raises: 
+        -NoConfigFoundError : When souce config is missing or either 
+                                table/database name is incorrect
+    """
+    config_base_path = "Personal\etlConfigs\source_config"
+    file_name = f'{table_name}.json' 
+    config_path = os.path.join(config_base_path, src_system, schema_name, file_name)
+    if not os.path.exists(config_path):
+        raise NoConfigFoundError()
+    with open(config_path) as F :
+        dict_config = json.loads(F.read())
+    return dict_config
